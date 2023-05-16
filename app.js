093ee8eec184ce915193339afaae72d7b25e5deb
Bhz91 <cardoso@et.esiea.fr>
2023-05-16 17:42:18 +0200

    mise en ligne d'un code pour afficher une carte en html

---
+++ app.js
@@ -0,0 +1,16 @@
+var map = L.map("map").setView([48.86284902002203, 2.3413018393060523], 13);
+
+var Stadia_OSMBright = L.tileLayer(
+  "https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png",
+  {
+    maxZoom: 20,
+    attribution:
+      '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+  }
+);
+var secondLayer = L.tileLayer('C:\Users\Cardoso\Desktop\20220212_contact_leaflet_map-main\visuel-atelier-cartes-fantasy2.png', {
+  attribution: 'Map data © Second Source'
+}).addTo(map);
+
+
+var marker = L.marker([48.86284902002203, 2.3413018393060523]).addTo(map);
